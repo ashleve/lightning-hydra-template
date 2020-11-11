f88ea00930d0f456cd82c34ada65f8b34f45fafc
hobogalaxy <zalewski.ukas@gmail.com>
2020-11-11 04:28:06 +0100

    add ImagePredictionLoggerCallback

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,4 +1,5 @@
 from pytorch_lightning.callbacks import Callback
+import torch
 import wandb
 import os
 
@@ -33,3 +34,21 @@ class SaveOnnxToWandbCallback(Callback):
         file_path = os.path.join(self.save_dir, "model.onnx")
         pl_module.to_onnx(file_path=file_path, input_sample=self.dummy_input.to(pl_module.device))
         wandb.save(file_path)
+
+
+class ImagePredictionLoggerCallback(Callback):
+    def __init__(self, val_samples, num_samples=32):
+        super().__init__()
+        self.val_imgs, self.val_labels = val_samples[:num_samples]
+
+    def on_validation_epoch_end(self, trainer, pl_module):
+        val_imgs = self.val_imgs.to(device=pl_module.device)
+
+        logits = pl_module(val_imgs)
+        preds = torch.argmax(logits, -1)
+
+        trainer.logger.experiment.log({
+            "examples": [wandb.Image(x, caption=f"Pred:{pred}, Label:{y}")
+                         for x, pred, y in zip(val_imgs, preds, self.val_labels)],
+            "global_step": trainer.global_step
+        })
