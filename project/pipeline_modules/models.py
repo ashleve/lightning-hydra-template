e694cd0164e92dee9a451b8df20260fdf9bec4e2
hobogalaxy <zalewski.ukas@gmail.com>
2020-11-16 04:46:16 +0100

    add EfficientNetPretrained model

--- project/pipeline_modules/models.py
+++ project/pipeline_modules/models.py
@@ -1,32 +1,12 @@
-from torch import nn
+from pl_bolts.models.self_supervised import CPCV2
+from efficientnet_pytorch import EfficientNet
 import torch.nn.functional as F
+from torchvision import models
+from torch import nn
+import torch
 
 
-class ModelMNISTv1(nn.Module):
-
-    def __init__(self, config):
-        super().__init__()
-
-        # mnist images are (1, 28, 28) (channels, width, height)
-        self.layer_1 = nn.Linear(28 * 28, config["lin1_size"])
-        self.layer_2 = nn.Linear(config["lin1_size"], config["lin2_size"])
-        self.layer_3 = nn.Linear(config["lin2_size"], 10)
-
-    def forward(self, x):
-        batch_size, channels, width, height = x.size()
-
-        # (b, 1, 28, 28) -> (b, 1*28*28)
-        x = x.view(batch_size, -1)
-        x = self.layer_1(x)
-        x = F.relu(x)
-        x = self.layer_2(x)
-        x = F.relu(x)
-        x = self.layer_3(x)
-
-        return F.log_softmax(x, dim=1)
-
-
-class ModelMNISTv2(nn.Module):
+class SimpleLinearMNIST(nn.Module):
 
     def __init__(self, config):
         super().__init__()
@@ -51,3 +31,31 @@ class ModelMNISTv2(nn.Module):
         x = self.layer_4(x)
 
         return F.log_softmax(x, dim=1)
+
+
+class EfficientNetPretrained(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+
+        self.model = EfficientNet.from_pretrained('efficientnet-b1')
+        # self.model = EfficientNet.from_pretrained('efficientnet-b7')
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        self.model._fc = nn.Linear(self.model._fc.in_features, config["lin1_size"])
+        self.drop1 = nn.Dropout(p=0.25)
+        self.lin_1 = nn.Linear(config["lin1_size"], config["lin2_size"])
+        self.drop2 = nn.Dropout(p=0.2)
+        self.lin_2 = nn.Linear(config["lin2_size"], config["output_size"])
+
+    def forward(self, x):
+        x = self.model(x)
+        x = F.relu(x)
+        x = self.drop1(x)
+        x = self.lin_1(x)
+        x = F.relu(x)
+        x = self.drop2(x)
+        x = self.lin_2(x)
+        return torch.sigmoid(x)
