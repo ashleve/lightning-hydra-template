41b66d43b7e1c58e82994f2cee69eca5c01bd2f0
hobogalaxy <zalewski.ukas@gmail.com>
2020-12-08 00:16:28 +0100

    refactor train.py

--- project/train.py
+++ project/train.py
@@ -1,4 +1,3 @@
-from pytorch_lightning.profiler import SimpleProfiler
 from pytorch_lightning.loggers import WandbLogger
 from argparse import ArgumentParser
 import pytorch_lightning as pl
@@ -7,8 +6,7 @@ import yaml
 import os
 
 # utils
-from utils.init_utils import init_lit_model, init_data_module, init_main_callbacks, init_wandb_logger
-from utils.callbacks import *
+from utils.init_utils import init_lit_model, init_data_module, init_trainer, init_callbacks, init_wandb_logger
 
 
 def train(project_config: dict, run_config: dict, use_wandb: bool):
@@ -27,55 +25,19 @@ def train(project_config: dict, run_config: dict, use_wandb: bool):
         log_path=os.path.join(os.path.dirname(__file__), "logs/")
     ) if use_wandb else None
 
-    # Init ModelCheckpoint and EarlyStopping callbacks
-    callbacks: List[pl.Callback] = init_main_callbacks(project_config=project_config)
-
-    # Add custom callbacks from utils/callbacks.py
-    callbacks.extend([
-        # MetricsHeatmapLoggerCallback(),
-        # UnfreezeModelCallback(wait_epochs=5),
-    ])
-    if use_wandb:
-        callbacks.append(
-            SaveCodeToWandbCallback(
-                base_dir=os.path.dirname(__file__),
-                wandb_save_dir=logger.save_dir,
-                run_config=run_config
-            ),
-        )
-
-    # Get path to checkpoint you want to resume with if it was set in the run config
-    resume_from_checkpoint = run_config.get("resume_training", {}).get("checkpoint_path", None)
+    # Init callbacks
+    callbacks: List[pl.Callback] = init_callbacks(
+        project_config=project_config,
+        run_config=run_config,
+        use_wandb=use_wandb
+    )
 
     # Init PyTorch Lightning trainer ⚡
-    trainer = pl.Trainer(
-        # whether to use gpu and how many
-        gpus=project_config["num_of_gpus"],
-
-        # experiment logging
+    trainer: pl.Trainer = init_trainer(
+        project_config=project_config,
+        run_config=run_config,
         logger=logger,
-
-        # useful callbacks
-        callbacks=callbacks,
-
-        # resume training from checkpoint if it was set in the run config
-        resume_from_checkpoint=resume_from_checkpoint
-        if resume_from_checkpoint != "None" and resume_from_checkpoint is not False
-        else None,
-
-        # print related
-        progress_bar_refresh_rate=project_config["printing"]["progress_bar_refresh_rate"],
-        profiler=SimpleProfiler() if project_config["printing"]["profiler"] else None,
-        weights_summary=project_config["printing"]["weights_summary"],
-
-        # number of validation sanity checks
-        num_sanity_val_steps=3,
-
-        # default log dir if no logger is found
-        default_root_dir=os.path.dirname(__file__) + "logs/lightning_logs",
-
-        # insert all other trainer parameters specified in run config
-        **run_config["trainer"]
+        callbacks=callbacks
     )
 
     # Evaluate model on test set before training
@@ -100,14 +62,6 @@ def main(run_config_name: str, use_wandb: bool):
     project_config: dict = load_config("project_config.yaml")
     run_config: dict = load_config("run_configs.yaml")[run_config_name]
 
-    # Print run config info
-    print("EXECUTING RUN:", run_config_name)
-    print("CONFIG:")
-    for section in run_config:
-        print("  " + section + ":")
-        for key in run_config[section]:
-            print("    " + key + ":", run_config[section][key])
-
     # Train model
     train(project_config=project_config, run_config=run_config, use_wandb=use_wandb)
 
@@ -115,7 +69,8 @@ def main(run_config_name: str, use_wandb: bool):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-r", "--run_config", type=str, default="MNIST_CLASSIFIER_V1")
-    parser.add_argument("-u", "--use_wandb", type=bool, default=True)
+    parser.add_argument("-n", "--no_wandb", dest='use_wandb', action='store_false')
+    parser.set_defaults(use_wandb=True)
     args = parser.parse_args()
 
     main(run_config_name=args.run_config, use_wandb=args.use_wandb)
--- project/utils/init_utils.py
+++ project/utils/init_utils.py
@@ -1,30 +1,78 @@
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.profiler import SimpleProfiler
 from pytorch_lightning.loggers import WandbLogger
 import pytorch_lightning as pl
 from typing import List
+import utils.callbacks
 import importlib
 import os
 
 
 def init_lit_model(hparams: dict) -> pl.LightningModule:
     """Load LitModel from folder specified in run config."""
+
     module_path = "models." + hparams["model_folder"] + ".lightning_module"
     lit_model = importlib.import_module(module_path).LitModel(hparams=hparams)
+
     return lit_model
 
 
 def init_data_module(hparams: dict) -> pl.LightningDataModule:
     """Load DataModule from folder specified in run config."""
+
     module_path = "data_modules." + hparams["datamodule_folder"] + ".datamodule"
     hparams["data_dir"] = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
     datamodule = importlib.import_module(module_path).DataModule(hparams=hparams)
     datamodule.prepare_data()
     datamodule.setup()
+
     return datamodule
 
 
-def init_main_callbacks(project_config: dict) -> List[pl.Callback]:
-    """Initialize ModelCheckpoint and EarlyStopping callbacks."""
+def init_trainer(project_config: dict, run_config: dict, logger, callbacks: list) -> pl.Trainer:
+    """Initialize PyTorch Lightning Trainer"""
+
+    # Get path to checkpoint you want to resume with if it was set in the run config
+    resume_from_checkpoint = run_config.get("resume_training", {}).get("checkpoint_path", None)
+
+    trainer = pl.Trainer(
+        # whether to use gpu and how many
+        gpus=project_config["num_of_gpus"],
+
+        # experiment logging
+        logger=logger,
+
+        # useful callbacks
+        callbacks=callbacks,
+
+        # resume training from checkpoint if it was set in the run config
+        resume_from_checkpoint=resume_from_checkpoint
+        if resume_from_checkpoint != "None"
+        and resume_from_checkpoint != "False"
+        and resume_from_checkpoint is not False
+        else None,
+
+        # print related
+        progress_bar_refresh_rate=project_config["printing"]["progress_bar_refresh_rate"],
+        profiler=SimpleProfiler() if project_config["printing"]["profiler"] else None,
+        weights_summary=project_config["printing"]["weights_summary"],
+
+        # number of validation sanity checks
+        num_sanity_val_steps=3,
+
+        # default log dir if no logger is found
+        default_root_dir=os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs/lightning_logs"),
+
+        # insert all other trainer parameters specified in run config
+        **run_config["trainer"]
+    )
+
+    return trainer
+
+
+def init_callbacks(project_config: dict, run_config: dict, use_wandb: bool) -> List[pl.Callback]:
+    """Initialize default callbacks ana callbacks specified in run config."""
+
     callbacks = [
         ModelCheckpoint(
             monitor=project_config["callbacks"]["checkpoint"]["monitor"],
@@ -38,11 +86,27 @@ def init_main_callbacks(project_config: dict) -> List[pl.Callback]:
             mode=project_config["callbacks"]["early_stop"]["mode"],
         )
     ]
+    if use_wandb:
+        callbacks.append(
+            utils.callbacks.SaveCodeToWandbCallback(
+                base_dir=os.path.dirname(os.path.dirname(__file__)),
+                wandb_save_dir=os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs"),
+                run_config=run_config
+            )
+        )
+
+    callbacks_config = run_config.get("callbacks", {})
+    for conf in callbacks_config:
+        callback = getattr(utils.callbacks, conf)
+        callbacks.append(callback(**callbacks_config[conf]))
+
     return callbacks
 
 
-def init_wandb_logger(project_config: dict, run_config: dict,
-                      lit_model: pl.LightningModule, datamodule: pl.LightningDataModule,
+def init_wandb_logger(project_config: dict,
+                      run_config: dict,
+                      lit_model: pl.LightningModule,
+                      datamodule: pl.LightningDataModule,
                       log_path: str = "logs/") -> pl.loggers.WandbLogger:
     """Initialize Weights&Biases logger."""
 
@@ -83,7 +147,7 @@ def init_wandb_logger(project_config: dict, run_config: dict,
         wandb_logger.watch(lit_model, log=None)
 
     wandb_logger.log_hyperparams({
-        "model_name": lit_model.model.__class__.__name__,
+        "model": lit_model.model.__class__.__name__,
         "optimizer": lit_model.configure_optimizers().__class__.__name__,
         "train_size": len(datamodule.data_train)
         if hasattr(datamodule, 'data_train') and datamodule.data_train is not None else 0,
@@ -95,9 +159,10 @@ def init_wandb_logger(project_config: dict, run_config: dict,
     wandb_logger.log_hyperparams(run_config["trainer"])
     wandb_logger.log_hyperparams(run_config["model"])
     wandb_logger.log_hyperparams(run_config["dataset"])
+
     return wandb_logger
 
 
-def init_tensorboard_loggger() -> pl.loggers.TensorBoardLogger:
+def init_tensorboard_logger() -> pl.loggers.TensorBoardLogger:
     """TODO"""
     return None
