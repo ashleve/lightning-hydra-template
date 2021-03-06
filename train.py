c26c73cc2d9ab4274bb8cea024cd4b3aed93c215
hobogalaxy <zalewski.ukas@gmail.com>
2021-03-06 14:31:27 +0100

    add disabling warnings and logs to main config

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# pytorch lightning imports
+# lightning imports
 from pytorch_lightning import LightningModule, LightningDataModule, Callback, Trainer
 from pytorch_lightning.loggers import LightningLoggerBase
 from pytorch_lightning import seed_everything
@@ -9,6 +9,8 @@ import hydra
 
 # normal imports
 from typing import List
+import warnings
+import logging
 
 # src imports
 from src.utils import template_utils as utils
@@ -16,39 +18,47 @@ from src.utils import template_utils as utils
 
 def train(config: DictConfig):
 
+    # Disable python warnings
+    if config['disable_warnings']:
+        warnings.filterwarnings('ignore')
+
+    # Disable PyTorch Lightning logs
+    if config['disable_lightning_logs']:
+        logging.getLogger('lightning').setLevel(logging.ERROR)
+
     # Pretty print config using Rich library
-    if config["print_config"]:
+    if config['print_config']:
         utils.print_config(config)
 
     # Set seed for random number generators in pytorch, numpy and python.random
-    if "seed" in config:
-        seed_everything(config["seed"])
+    if 'seed' in config:
+        seed_everything(config['seed'])
 
     # Init PyTorch Lightning model ⚡
-    model: LightningModule = hydra.utils.instantiate(config["model"])
+    model: LightningModule = hydra.utils.instantiate(config['model'])
 
     # Init PyTorch Lightning datamodule ⚡
-    datamodule: LightningDataModule = hydra.utils.instantiate(config["datamodule"])
+    datamodule: LightningDataModule = hydra.utils.instantiate(config['datamodule'])
     datamodule.prepare_data()
     datamodule.setup()
 
-    # Init PyTorch Lightning callbacks ⚡
+    # Init PyTorch Lightning callbacks
     callbacks: List[Callback] = []
-    if "callbacks" in config:
-        for _, cb_conf in config["callbacks"].items():
-            if "_target_" in cb_conf:
+    if 'callbacks' in config:
+        for _, cb_conf in config['callbacks'].items():
+            if '_target_' in cb_conf:
                 callbacks.append(hydra.utils.instantiate(cb_conf))
 
-    # Init PyTorch Lightning loggers ⚡
+    # Init PyTorch Lightning loggers
     logger: List[LightningLoggerBase] = []
-    if "logger" in config:
-        for _, lg_conf in config["logger"].items():
-            if "_target_" in lg_conf:
+    if 'logger' in config:
+        for _, lg_conf in config['logger'].items():
+            if '_target_' in lg_conf:
                 logger.append(hydra.utils.instantiate(lg_conf))
 
     # Init PyTorch Lightning trainer ⚡
     trainer: Trainer = hydra.utils.instantiate(
-        config["trainer"], callbacks=callbacks, logger=logger
+        config['trainer'], callbacks=callbacks, logger=logger
     )
 
     # Send some parameters from config to all lightning loggers
@@ -61,7 +71,7 @@ def train(config: DictConfig):
         logger=logger,
     )
 
-    # Train the model
+    # Train the model ⚡
     trainer.fit(model=model, datamodule=datamodule)
 
     # Evaluate model on test set after training
@@ -77,16 +87,16 @@ def train(config: DictConfig):
         logger=logger,
     )
 
-    # Return best achieved metric score for optuna
-    optimized_metric = config.get("optimized_metric", None)
+    # Return metric score for optuna optimization
+    optimized_metric = config.get('optimized_metric', None)
     if optimized_metric:
         return trainer.callback_metrics[optimized_metric]
 
 
-@hydra.main(config_path="configs/", config_name="config.yaml")
+@hydra.main(config_path='configs/', config_name='config.yaml')
 def main(config: DictConfig):
     return train(config)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
