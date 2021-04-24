0fe22deafdad6aee6a7400ece17811cfa142a455
Tristan <t.payer@gmx.de>
2021-04-24 23:02:05 +0200

    Added image logger callback PR improvements

--- src/callbacks/wandb_callbacks.py
+++ src/callbacks/wandb_callbacks.py
@@ -236,21 +236,4 @@ class ImagePredictionLogger(Callback):
                           for x, pred, y in zip(val_imgs[:self.num_samples],
                                                 preds[:self.num_samples],
                                                 val_labels[:self.num_samples])]
-=======
-        # get a validation batch from the validation dataloader
-        val_samples = next(iter(trainer.datamodule.val_dataloader()))
-        val_imgs, val_labels = val_samples
-
-        # run the batch through the network
-        val_imgs = val_imgs.to(device=pl_module.device)
-        logits = pl_module(val_imgs)
-        preds = torch.argmax(logits, axis=-1)
-
-        # log the images as wandb Image
-        trainer.logger.experiment[0].log({
-            'Images':[wandb.Image(x, caption=f'Pred:{pred}, Label:{y}')
-            for x, pred, y in zip(val_imgs[:self.num_samples],
-                                preds[:self.num_samples],
-                                val_labels[:self.num_samples])]
->>>>>>> a4921ef30b9212be966e11470dceb9164be0cce2
             })
