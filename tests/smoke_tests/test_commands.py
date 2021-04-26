b1d8dc874210f84bcde054f14b82f815424062e3
ashleve <zalewski.ukas@gmail.com>
2021-04-26 17:23:47 +0200

    add smoke tests implemented with pytest

---
+++ tests/smoke_tests/test_commands.py
@@ -0,0 +1,103 @@
+from typing import List
+
+import pytest
+import sh
+
+
+def run_command(command: List[str]):
+    """Default method for executing shell commands."""
+    msg = None
+    try:
+        sh.python(command)
+    except sh.ErrorReturnCode as e:
+        msg = e.stderr.decode()
+    if msg:
+        pytest.fail(msg=msg)
+
+
+def test_default():
+    command = ["run.py", "trainer.max_epochs=1"]
+    run_command(command)
+
+
+def test_fast_dev_run():
+    """Run 1 train, val, test batch."""
+    command = ["run.py", "trainer=default", "trainer.fast_dev_run=true"]
+    run_command(command)
+
+
+def test_experiments():
+    """Train 1 epoch with all experiment configs."""
+    command = ["run.py", "-m", "experiment=glob(*)", "trainer.max_epochs=1"]
+    run_command(command)
+
+
+def test_limit_batches():
+    """Train 1 epoch on 25% of data."""
+    command = [
+        "run.py",
+        "trainer=default",
+        "trainer.max_epochs=1",
+        "trainer.limit_train_batches=0.25",
+        "trainer.limit_val_batches=0.25",
+        "trainer.limit_test_batches=0.25",
+    ]
+    run_command(command)
+
+
+def test_gradient_accumulation():
+    """Train 1 epoch with gradient accumulation."""
+    command = [
+        "run.py",
+        "trainer=default",
+        "trainer.max_epochs=1",
+        "trainer.accumulate_grad_batches=10",
+    ]
+    run_command(command)
+
+
+def test_double_validation_loop():
+    """Train 1 epoch with validation loop twice per epoch."""
+    command = [
+        "run.py",
+        "trainer=default",
+        "trainer.max_epochs=1",
+        "trainer.val_check_interval=0.5",
+    ]
+    run_command(command)
+
+
+def test_csv_logger():
+    """Train 5 epochs with 5 batches with CSVLogger."""
+    command = [
+        "run.py",
+        "trainer=default",
+        "trainer.max_epochs=5",
+        "trainer.limit_train_batches=5",
+        "logger=csv",
+    ]
+    run_command(command)
+
+
+def test_tensorboard():
+    """Train 5 epochs with 5 batches with TensorboardLogger."""
+    command = [
+        "run.py",
+        "trainer=default",
+        "trainer.max_epochs=5",
+        "trainer.limit_train_batches=5",
+        "logger=tensorboard",
+    ]
+    run_command(command)
+
+
+def test_overfit_batches():
+    """Overfit to 10 batches over 10 epochs."""
+    command = [
+        "run.py",
+        "trainer=default",
+        "trainer.min_epochs=10",
+        "trainer.max_epochs=10",
+        "trainer.overfit_batches=10",
+    ]
+    run_command(command)
