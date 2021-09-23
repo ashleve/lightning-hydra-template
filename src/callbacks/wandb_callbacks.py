fix incorrect calculation of precision, recall and f1 score in wandb callback

src/callbacks/wandb_callbacks.py
@@ -207,9 +207,9 @@ class LogF1PrecRecHeatmap(Callback):
 
             preds = torch.cat(self.preds).cpu().numpy()
             targets = torch.cat(self.targets).cpu().numpy()
-            f1 = f1_score(preds, targets, average=None)
-            r = recall_score(preds, targets, average=None)
-            p = precision_score(preds, targets, average=None)
+            f1 = f1_score(targets, preds, average=None)
+            r = recall_score(targets, preds, average=None)
+            p = precision_score(targets, preds, average=None)
             data = [f1, p, r]
 
             # set figure size