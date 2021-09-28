4d7de7c47c3c65862c2d8b939c09e154cd2f4afb
ashleve <zalewski.ukas@gmail.com>
2021-09-28 17:27:22 +0200

    update setup_conda.sh

--- bash/setup_conda.sh
+++ bash/setup_conda.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
-# Run from root folder with: bash bash/setup_conda.sh
 
-# check if conda is installed
+
+# Check if conda is installed
 if ! command -v conda &> /dev/null
 then
     echo "The 'conda' command could not be found. Exiting..."
     exit
 fi
 
-# This line is needed for enabling conda env activation
-source ~/miniconda3/etc/profile.d/conda.sh
 
-# Configure conda env
-read -rp "Enter environment name: " env_name
+# Configure env
+read -rp "Enter conda environment name: " env_name
 read -rp "Enter python version (recommended '3.8') " python_version
-read -rp "Enter cuda version (recommended '10.2', or 'none' for CPU only): " cuda_version
-read -rp "Enter pytorch version (recommended '1.8.1'): " pytorch_version
+read -rp "Enter cuda version ('10.2', '11.1' or 'none' for CPU only): " cuda_version
 
-# Create conda env
+
+# Create env
 conda create -y -n "$env_name" python="$python_version"
-conda activate "$env_name"
 
-# Install pytorch
+
+# Install pytorch + cuda
 if [ "$cuda_version" == "none" ]; then
-    conda install -y pytorch=$pytorch_version torchvision torchaudio cpuonly -c pytorch
+    conda install -n "$env_name" -y pytorch torchvision torchaudio cpuonly -c pytorch
+elif [ "$cuda_version" == "10.2" ]; then
+    conda install -n "$env_name" pytorch torchvision torchaudio cudatoolkit=$cuda_version -c pytorch
+elif [ "$cuda_version" == "11.1" ]; then
+    conda install -n "$env_name" pytorch torchvision torchaudio cudatoolkit=$cuda_version -c pytorch -c nvidia
 else
-    conda install -y pytorch=$pytorch_version torchvision torchaudio cudatoolkit=$cuda_version -c pytorch
+    echo "Incorrect cuda version. Exiting..."
+    exit
 fi
 
-echo "\n"
-echo "To activate this environment, use:"
+
+# Final message
+echo "======================================="
+echo "To activate this environment use:"
 echo "conda activate $env_name"
+echo "======================================="
+echo -e "\a"
