93b7c406d9fd76e1266a409246ae2e5b2b61acc1
ashleve <zalewski.ukas@gmail.com>
2021-11-04 18:44:56 +0100

    Add specifying model and datamodule during `trainer.test()`

--- src/train.py
+++ src/train.py
@@ -76,10 +76,10 @@ def train(config: DictConfig) -> Optional[float]:
     log.info("Starting training!")
     trainer.fit(model=model, datamodule=datamodule)
 
-    # Evaluate model on test set, using the best model achieved during training
+    # Evaluate on test set, using the best model weights achieved during training
     if config.get("test_after_training") and not config.trainer.get("fast_dev_run"):
         log.info("Starting testing!")
-        trainer.test()
+        trainer.test(model=model, datamodule=datamodule, ckpt_path="best")
 
     # Make sure everything closed properly
     log.info("Finalizing!")
