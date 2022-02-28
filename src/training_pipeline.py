5b7fde8e3a32fbf3b0867fe41cbbf3c8298fb779
LEE HANBIN <hanbin@kakao.com>
2022-03-01 06:23:36 +0900

    fix : Not printing best ckeckpoint (#261)
    * change `config.trainer.get("train")` to `config.get("train")` in `training_pipeline.py`

--- src/training_pipeline.py
+++ src/training_pipeline.py
@@ -114,7 +114,7 @@ def train(config: DictConfig) -> Optional[float]:
     )
 
     # Print path to best checkpoint
-    if not config.trainer.get("fast_dev_run") and config.trainer.get("train"):
+    if not config.trainer.get("fast_dev_run") and config.get("train"):
         log.info(f"Best model ckpt at {trainer.checkpoint_callback.best_model_path}")
 
     # Return metric score for hyperparameter optimization
