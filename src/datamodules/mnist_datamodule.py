679ea301c5c266b057dc0821808c9b587428a5ee
ashleve <zalewski.ukas@gmail.com>
2022-03-17 16:43:35 +0100

    Update datamodule dosctring

--- src/datamodules/mnist_datamodule.py
+++ src/datamodules/mnist_datamodule.py
@@ -62,9 +62,9 @@ class MNISTDataModule(LightningDataModule):
     def setup(self, stage: Optional[str] = None):
         """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
 
-        This method is called by lightning twice for `trainer.fit()` and `trainer.test()`,
-        so be careful if you do a random split! The `stage` can be used to differentiate
-        whether it's called before trainer.fit()` or `trainer.test()`.
+        This method is called by lightning when doing `trainer.fit()` and `trainer.test()`,
+        so be careful not to execute the random split twice! The `stage` can be used to
+        differentiate whether it's called before trainer.fit()` or `trainer.test()`.
         """
 
         # load datasets only if they're not loaded already
