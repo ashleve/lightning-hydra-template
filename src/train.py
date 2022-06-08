b5cd7e4cca0288ebb7786ebd8aa30ebb9ae7c374
ashleve <zalewski.ukas@gmail.com>
2022-06-08 16:27:12 +0200

    Disable changing work dir by pyrootuils by default

--- src/train.py
+++ src/train.py
@@ -3,12 +3,18 @@ import pyrootutils
 from omegaconf import DictConfig
 
 # project root setup (allows for calling this script from any place)
-# searches for root indicators in parent directories, like ".git", "pyproject.toml", "setup.py", etc.
-# adds project root directory to the PYTHONPATH
-# sets current working directory to the root directory
+# searches for root indicator in parent directories, like ".git", "pyproject.toml", "setup.py", etc.
+# sets PROJECT_ROOT environment variable (used for specifying paths in hydra config)
 # loads environment variables from ".env" file if exists
+# adds project root directory to the PYTHONPATH (so imports work correctly)
 # https://github.com/ashleve/pyrootutils
-root = pyrootutils.setup_root(__file__)
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    project_root_env_var=True,
+    dotenv=True,
+    pythonpath=True,
+    cwd=False,  # do NOT change working directory to root (would cause problems in DDP mode)
+)
 
 
 @hydra.main(version_base="1.2", config_path=root / "configs", config_name="train.yaml")
