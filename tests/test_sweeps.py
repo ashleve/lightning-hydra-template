429ef4c3706b3f7335aa831190769d785bcca7bf
ashleve <zalewski.ukas@gmail.com>
2022-07-09 18:56:53 +0200

    Update

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -4,7 +4,7 @@ from tests.helpers.run_if import RunIf
 from tests.helpers.run_sh_command import run_sh_command
 
 startfile = "src/train.py"
-overrides = ["++trainer.fast_dev_run=true", "logger=[]"]
+overrides = ["logger=[]"]
 
 
 @RunIf(sh=True)
@@ -16,29 +16,48 @@ def test_experiments(tmp_path):
         "-m",
         "experiment=glob(*)",
         "hydra.sweep.dir=" + str(tmp_path),
+        "++trainer.fast_dev_run=true",
     ] + overrides
     run_sh_command(command)
 
 
 @RunIf(sh=True)
 @pytest.mark.slow
-def test_default_sweep(tmp_path):
-    """Test default hydra sweeper."""
+def test_hydra_sweep(tmp_path):
+    """Test default hydra sweep."""
     command = [
         startfile,
         "-m",
         "hydra.sweep.dir=" + str(tmp_path),
-        "model.optimizer.lr=0.01,0.02,0.03",
-        "trainer=default",
+        "model.optimizer.lr=0.005,0.01",
+        "++trainer.fast_dev_run=true",
     ] + overrides
 
     run_sh_command(command)
 
 
+@RunIf(wandb=True, sh=True)
+@pytest.mark.slow
+def test_hydra_sweep_ddp_sim(tmp_path):
+    """Test default hydra sweep with ddp sim."""
+    command = [
+        startfile,
+        "-m",
+        "hydra.sweep.dir=" + str(tmp_path),
+        "trainer=ddp_sim",
+        "trainer.max_epochs=3",
+        "+trainer.limit_train_batches=0.01",
+        "+trainer.limit_val_batches=0.1",
+        "+trainer.limit_test_batches=0.1",
+        "model.optimizer.lr=0.005,0.01,0.02",
+    ] + overrides
+    run_sh_command(command)
+    
+
 @RunIf(sh=True)
 @pytest.mark.slow
 def test_optuna_sweep(tmp_path):
-    """Test optuna sweeper."""
+    """Test optuna sweep."""
     command = [
         startfile,
         "-m",
@@ -46,12 +65,14 @@ def test_optuna_sweep(tmp_path):
         "hydra.sweep.dir=" + str(tmp_path),
         "hydra.sweeper.n_trials=10",
         "hydra.sweeper.sampler.n_startup_trials=5",
+        "++trainer.fast_dev_run=true",
     ] + overrides
     run_sh_command(command)
 
 
 @RunIf(wandb=True, sh=True)
-def test_optuna_wandb_ddp_sim(tmp_path):
+@pytest.mark.slow
+def test_optuna_sweep_ddp_sim_wandb(tmp_path):
     """Test optuna sweep with wandb and ddp sim."""
     command = [
         startfile,
@@ -59,11 +80,11 @@ def test_optuna_wandb_ddp_sim(tmp_path):
         "hparams_search=mnist_optuna",
         "hydra.sweep.dir=" + str(tmp_path),
         "hydra.sweeper.n_trials=5",
-        "logger=wandb",
         "trainer=ddp_sim",
         "trainer.max_epochs=3",
         "+trainer.limit_train_batches=0.01",
         "+trainer.limit_val_batches=0.1",
         "+trainer.limit_test_batches=0.1",
+        "logger=wandb",
     ]
     run_sh_command(command)
