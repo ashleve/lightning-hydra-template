66a3fb7be4d96388fd279a4dd6171cea12b25327
ashleve <zalewski.ukas@gmail.com>
2022-07-09 20:28:54 +0200

    Add setup.py (#369)

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Describe Your Cool Project",
+    author="",
+    author_email="",
+    url="https://github.com/user/project",  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
+    install_requires=["pytorch-lightning", "hydra-core"],
+    packages=find_packages(),
+)
