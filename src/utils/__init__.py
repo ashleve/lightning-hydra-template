2cbb7016f21cc75666ec8c27132111da1ba62457
ashleve <zalewski.ukas@gmail.com>
2022-07-11 16:35:23 +0200

    Add missing function imports to `__init__.py`

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,12 @@
 from src.utils.pylogger import get_pylogger
 from src.utils.rich_utils import enforce_tags, print_config_tree
 from src.utils.task_utils import (
+    close_loggers,
+    extras,
     get_metric_value,
     instantiate_callbacks,
     instantiate_loggers,
     log_hyperparameters,
+    save_file,
     task_wrapper,
 )
