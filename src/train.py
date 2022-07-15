7b2cb6c0cd1e1651abb99dd11c219a56732eb50a
ashleve <ashlevegalaxy@gmail.com>
2022-07-15 18:08:39 +0200

    Construct config path from root (#387)

--- src/train.py
+++ src/train.py
@@ -11,10 +11,10 @@ from omegaconf import DictConfig
 # loads environment variables from ".env" if exists
 # adds root dir to the PYTHONPATH (so this file can be run from any place)
 # https://github.com/ashleve/pyrootutils
-pyrootutils.setup_root(__file__, dotenv=True, pythonpath=True)
+root = pyrootutils.setup_root(__file__, dotenv=True, pythonpath=True)
 
 
-@hydra.main(version_base="1.2", config_path="../configs", config_name="train.yaml")
+@hydra.main(version_base="1.2", config_path=root / "configs", config_name="train.yaml")
 def main(cfg: DictConfig) -> float:
 
     # imports can be nested inside @hydra.main to optimize tab completion
