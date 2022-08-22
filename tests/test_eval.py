27b13929570b37fbf0da6ae0aede2dd6aa03094c
ashleve <ashlevegalaxy@gmail.com>
2022-08-22 13:53:18 +0200

    Move tasks code inside entry files (#421)

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -4,7 +4,8 @@ import pytest
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import open_dict
 
-from src.tasks import eval_task, train_task
+from src.eval import evaluate
+from src.train import train
 
 
 @pytest.mark.slow
@@ -17,7 +18,7 @@ def test_train_eval(tmp_path, cfg_train, cfg_eval):
         cfg_train.test = True
 
     HydraConfig().set_config(cfg_train)
-    train_metric_dict, _ = train_task.train(cfg_train)
+    train_metric_dict, _ = train(cfg_train)
 
     assert "last.ckpt" in os.listdir(tmp_path / "checkpoints")
 
@@ -25,7 +26,7 @@ def test_train_eval(tmp_path, cfg_train, cfg_eval):
         cfg_eval.ckpt_path = str(tmp_path / "checkpoints" / "last.ckpt")
 
     HydraConfig().set_config(cfg_eval)
-    test_metric_dict, _ = eval_task.evaluate(cfg_eval)
+    test_metric_dict, _ = evaluate(cfg_eval)
 
     assert test_metric_dict["test/acc"] > 0.0
     assert abs(train_metric_dict["test/acc"].item() - test_metric_dict["test/acc"].item()) < 0.001
