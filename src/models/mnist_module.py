84dd08c05f0e86e032ad5012298dad57e6bbcd3b
ashleve <lukasz.zalewski.ai@gmail.com>
2022-12-07 15:32:35 +0100

    Add `task` arg to accuracy metric

--- src/models/mnist_module.py
+++ src/models/mnist_module.py
@@ -39,9 +39,9 @@ class MNISTLitModule(LightningModule):
         self.criterion = torch.nn.CrossEntropyLoss()
 
         # metric objects for calculating and averaging accuracy across batches
-        self.train_acc = Accuracy()
-        self.val_acc = Accuracy()
-        self.test_acc = Accuracy()
+        self.train_acc = Accuracy(task="multiclass")
+        self.val_acc = Accuracy(task="multiclass")
+        self.test_acc = Accuracy(task="multiclass")
 
         # for averaging loss across batches
         self.train_loss = MeanMetric()
