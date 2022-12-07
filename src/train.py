e6de27223d2e17a5368a2d7ddde7101dd34e3d83
ashleve <lukasz.zalewski.ai@gmail.com>
2022-12-07 16:01:35 +0100

    Fix formatting

--- src/train.py
+++ src/train.py
@@ -123,7 +123,9 @@ def main(cfg: DictConfig) -> Optional[float]:
     metric_dict, _ = train(cfg)
 
     # safely retrieve metric value for hydra-based hyperparameter optimization
-    metric_value = utils.get_metric_value(metric_dict=metric_dict, metric_name=cfg.get("optimized_metric"))
+    metric_value = utils.get_metric_value(
+        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
+    )
 
     # return optimized metric
     return metric_value
