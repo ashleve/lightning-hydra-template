18b145102a8d51034575adc7dabccdf3e5edb75a
Lukas <lukasz.zalewski.ai@gmail.com>
2022-12-11 21:30:04 +0100

    Upgrade to hydra 1.3 (#480)

--- src/train.py
+++ src/train.py
@@ -115,7 +115,7 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
     return metric_dict, object_dict
 
 
-@hydra.main(version_base="1.2", config_path=root / "configs", config_name="train.yaml")
+@hydra.main(version_base="1.3", config_path="../configs", config_name="train.yaml")
 def main(cfg: DictConfig) -> Optional[float]:
 
     # train the model
