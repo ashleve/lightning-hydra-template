6fcf61975bd491a4448cf1eba69a67e018fb6305
Lukas <lukasz.zalewski.ai@gmail.com>
2023-01-11 20:34:27 +0100

    Refactor tests (#498)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+"""This file prepares config fixtures for other tests."""
+
 import pyrootutils
 import pytest
 from hydra import compose, initialize
@@ -12,7 +14,7 @@ def cfg_train_global() -> DictConfig:
 
         # set defaults for all tests
         with open_dict(cfg):
-            cfg.paths.root_dir = str(pyrootutils.find_root())
+            cfg.paths.root_dir = str(pyrootutils.find_root(indicator=".project-root"))
             cfg.trainer.max_epochs = 1
             cfg.trainer.limit_train_batches = 0.01
             cfg.trainer.limit_val_batches = 0.1
@@ -35,7 +37,7 @@ def cfg_eval_global() -> DictConfig:
 
         # set defaults for all tests
         with open_dict(cfg):
-            cfg.paths.root_dir = str(pyrootutils.find_root())
+            cfg.paths.root_dir = str(pyrootutils.find_root(indicator=".project-root"))
             cfg.trainer.max_epochs = 1
             cfg.trainer.limit_test_batches = 0.1
             cfg.trainer.accelerator = "cpu"
