7eebe474e995e3b30a3a3dbccc26e02b9e8ba9e2
ashleve <lukasz.zalewski.ai@gmail.com>
2023-02-25 13:52:39 +0100

    add commands

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setup(
     description="Describe Your Cool Project",
     author="",
     author_email="",
-    url="https://github.com/user/project",  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
+    url="https://github.com/user/project",
     install_requires=["pytorch-lightning", "hydra-core"],
     packages=find_packages(),
+    # use this to customize global commands available in the terminal after installing the package
+    entry_points={"console_scripts": ["train_command = src.train:main", "eval_command = src.eval:main"]},
 )
