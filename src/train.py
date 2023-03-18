7be0664a7dc2e0b7517f7377286a84e6d8bd34b0
Lukas <lukasz.zalewski.ai@gmail.com>
2023-03-18 21:11:14 +0100

    Update `README.md` (#551)

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 from typing import List, Optional, Tuple
 
 import hydra
-import lightning as pl
+import lightning as L
 import pyrootutils
 import torch
 from lightning import Callback, LightningDataModule, LightningModule, Trainer
@@ -48,7 +48,7 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
 
     # set seed for random number generators in pytorch, numpy and python.random
     if cfg.get("seed"):
-        pl.seed_everything(cfg.seed, workers=True)
+        L.seed_everything(cfg.seed, workers=True)
 
     log.info(f"Instantiating datamodule <{cfg.data._target_}>")
     datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
