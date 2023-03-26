562d8d56154e1c7756aca0fd74c66457afcddf1e
Yunchong Gan <yunchong@pku.edu.cn>
2023-03-26 20:20:32 +0800

    Rename file

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
-from src.utils.instantiatiators import instantiate_callbacks, instantiate_loggers
+from src.utils.instantiators import instantiate_callbacks, instantiate_loggers
 from src.utils.logging_utils import log_hyperparameters
 from src.utils.pylogger import get_pylogger
 from src.utils.rich_utils import enforce_tags, print_config_tree
