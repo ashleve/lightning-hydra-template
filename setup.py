f4507c6cde556ccf57b20753d61ec05c308bceac
MoustHolmes <moust.holmes@gmail.com>
2023-03-27 17:34:55 +0200

    more daatpipe

--- setup.py
+++ setup.py
@@ -2,13 +2,31 @@
 
 from setuptools import find_packages, setup
 
+INSTALL_REQUIRES = [
+    "dill>=0.3",
+    "hydra-core",
+    "matplotlib>=3.5",
+    "numpy>=1.21",
+    "pandas>=1.3",
+    "pyarrow",
+    "pydantic",
+    "pytorch-lightning",
+    "seaborn>=0.12.2",
+    "scikit_learn>=1.0",
+    "scipy>=1.7",
+    "sqlalchemy>=1.4",
+    "timer>=0.2",
+    "tqdm>=4.64",
+    "wandb>=0.12",
+]
+
 setup(
     name="src",
     version="0.0.1",
-    description="Describe Your Cool Project",
+    description="Developing a encoder transformer arcitekture for",
     author="",
     author_email="",
-    url="https://github.com/user/project",  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    install_requires=["pytorch-lightning", "hydra-core"],
+    url="https://github.com/MoustHolmes/IceCubeEncoderTransformer", 
+    install_requires=INSTALL_REQUIRES,
     packages=find_packages(),
 )
--- src/data/components/icecube_SQL_datapipe.py
+++ src/data/components/icecube_SQL_datapipe.py
@@ -1,11 +1,21 @@
 import sqlite3
+import pandas as pd
+import numpy as np
+import csv
+import random
+
 import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
+from torch.utils.data import DataLoader, random_split
+from torch.optim import Adam
 
-from torch.utils.data import Dataset
-from torch import Tensor
-from torchdata.datapipes.iter import  IterDataPipe, MaxTokenBucketizer
-from torch.nn.utils.rnn import pad_sequence
+import torchdata.datapipes as dp
+from torchdata.datapipes import functional_datapipe
+from torchdata.datapipes.iter import IterableWrapper, IterDataPipe, Mapper, MaxTokenBucketizer, ShardingFilter
 
+
+@functional_datapipe("read_csv")
 class ReadCSV(IterDataPipe):
     def __init__(self, csv_file):
         self.csv_file = csv_file
@@ -15,10 +25,23 @@ class ReadCSV(IterDataPipe):
             for line in f:
                 yield int(line.strip())
 
+@functional_datapipe("read_csv_dp")
+class ReadCSVMultiple(IterDataPipe):
+    def __init__(self, datapipe):
+        self.datapipe = datapipe
+
+    def __iter__(self):
+        for csv_file_path in self.datapipe:
+            with open(csv_file_path, "r") as f:
+                for line in f:
+                    yield int(line.strip())
+
+@functional_datapipe("query_sql")
 class QuerySQL(IterDataPipe):
-    def __init__(self, db_path, event_nos, input_cols, pulsemap, target_cols, truth_table):
+    def __init__(self, datapipe, db_path, input_cols, pulsemap, target_cols, truth_table):
+
+        self.datapipe = datapipe
         self.db_path = db_path
-        self.event_nos = event_nos
         self.input_cols_str = ", ".join(input_cols)
         self.target_cols_str = ", ".join(target_cols)
         self.pulsemap = pulsemap
@@ -26,14 +49,17 @@ class QuerySQL(IterDataPipe):
 
     def __iter__(self):
         with sqlite3.connect(self.db_path) as conn:
-            for event_no in self.event_nos:
-                features = Tensor(conn.execute(f"SELECT {self.input_cols_str} FROM {self.pulsemap} WHERE event_no == {event_no}").fetchall())
-                truth = Tensor(conn.execute(f"SELECT {self.target_cols_str} FROM {self.truth_table} WHERE event_no == {event_no}").fetchall())
+            for event_no in self.datapipe:
+                features = torch.Tensor(conn.execute(f"SELECT {self.input_cols_str} FROM {self.pulsemap} WHERE event_no == {event_no}").fetchall())
+                truth = torch.Tensor(conn.execute(f"SELECT {self.target_cols_str} FROM {self.truth_table} WHERE event_no == {event_no}").fetchall())
                 yield (features, truth)
 
+@functional_datapipe("transform_data")
 class TransfromData(IterDataPipe):
     def __init__(self, datapipe, feature_transform, truth_transform = None):
-        self.datapipe = datapipe
+        self.datapipe = datapipe 
+        # self.input_cols = input_cols
+        # self.target_cols = target_cols
         self.feature_transform = feature_transform
 
         if not truth_transform:
@@ -49,7 +75,22 @@ class TransfromData(IterDataPipe):
 
             yield (features, truth)
 
-def Prometheus_featuer_transform(features):
+
+def upgrade_feature_transform(features):
+    features[:, 0] = torch.log10(features[:, 0]) / 2.0  # charge
+    features[:, 1] /= 2e04  # dom_time
+    features[:, 1] -= 1.0
+    features[:, 2] /= 500.0  # dom_x
+    features[:, 3] /= 500.0  # dom_y
+    features[:, 4] /= 500.0  # dom_z
+    features[:, 5] /= 0.05  # pmt_area
+    # features[:,6] /= 1.  # pmt_dir_x
+    # features[:,7] /= 1.  # pmt_dir_y
+    # features[:,8] /= 1.  # pmt_dir_z
+    return features
+    
+
+def Prometheus_feature_transform(features):
     features[:, 0] /= 100.0  # dom_x
     features[:, 1] /= 100.0  # dom_y
     features[:, 2] += 350.0  # dom_z
@@ -62,6 +103,8 @@ def Prometheus_featuer_transform(features):
 def log10_target_transform(target):
    return torch.log10(target)
 
+
+@functional_datapipe("pad_batch")
 class PadBatch(IterDataPipe):
     def __init__(self, batch):
         self.batch = batch
@@ -84,3 +127,98 @@ class PadBatch(IterDataPipe):
 def len_fn(datapipe):
   features, _ = datapipe
   return features.shape[0]
+
+# def make_train_test_val_datapipe(
+#         csv_file, db_path, 
+#         input_cols, 
+#         pulsemap, 
+#         target_cols, 
+#         truth_table, 
+#         max_token_count,
+#         feature_transform,
+#         truth_transform = None
+# ):
+#     datapipe = ReadCSV( csv_file)
+#     datapipe = QuerySQL( db_path, datapipe, input_cols, pulsemap, target_cols, truth_table)
+#     datapipe = TransfromData( datapipe, feature_transform, truth_transform)
+#     datapipe = MaxTokenBucketizer( datapipe, max_token_count = max_token_count, len_fn = len_fn, include_padding = True)
+#     datapipe = PadBatch(datapipe)
+    
+def make_datapipe(
+        csv_file, 
+        db_path, 
+        input_cols, 
+        pulsemap, 
+        target_cols, 
+        truth_table, 
+        max_token_count,
+        feature_transform,
+        truth_transform = None
+    ):
+    datapipe = ReadCSV( csv_file).sharding_filter() \
+        .query_sql(
+        db_path = db_path, 
+        input_cols = input_cols, 
+        pulsemap = pulsemap, 
+        target_cols = target_cols,
+        truth_table = truth_table,
+        ) \
+        .transform_data(
+        feature_transform, 
+        truth_transform
+        ) \
+        .max_token_bucketize(
+        max_token_count = max_token_count,
+        len_fn = len_fn,
+        include_padding = True
+        ) \
+        .pad_batch()
+    return datapipe
+
+def make_train_test_val_datapipe(
+        train_csv_file,
+        test_csv_file,
+        val_csv_file,
+        db_path, 
+        input_cols, 
+        pulsemap, 
+        target_cols, 
+        truth_table, 
+        max_token_count,
+        feature_transform,
+        truth_transform = None
+    ):
+    train_datapipe = make_datapipe(
+        train_csv_file,
+        db_path,
+        input_cols,
+        pulsemap,
+        target_cols,
+        truth_table,
+        max_token_count,
+        feature_transform,
+        truth_transform
+    )
+    test_datapipe = make_datapipe(
+        test_csv_file,
+        db_path,
+        input_cols,
+        pulsemap,
+        target_cols,
+        truth_table,
+        max_token_count,
+        feature_transform,
+        truth_transform
+    )
+    val_datapipe = make_datapipe(
+        val_csv_file,
+        db_path,
+        input_cols,
+        pulsemap,
+        target_cols,
+        truth_table,
+        max_token_count,
+        feature_transform,
+        truth_transform
+    )
+    return train_datapipe, test_datapipe, val_datapipe
