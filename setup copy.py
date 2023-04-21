c320b7b2cb223e7a0f9613fed1f6a9c7ca1ed355
MoustHolmes <moust.holmes@gmail.com>
2023-04-21 14:20:40 +0200

    temo changes before merge with template

---
+++ setup copy.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = [
+    "dill>=0.3",
+    "hydra-core",
+    "matplotlib>=3.5",
+    "numpy>=1.21",
+    "pandas>=1.3",
+    "pyarrow",
+    "pydantic",
+    "pytorch-lightning",
+    "seaborn>=0.12.2",
+    "scikit_learn>=1.0",
+    "scipy>=1.7",
+    "sqlalchemy>=1.4",
+    "timer>=0.2",
+    "tqdm>=4.64",
+    "wandb>=0.12",
+]
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Developing a encoder transformer arcitekture for",
+    author="",
+    author_email="",
+    url="https://github.com/MoustHolmes/IceCubeEncoderTransformer", 
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+    # use this to customize global commands available in the terminal after installing the package
+    entry_points={
+        "console_scripts": [
+            "train_command = src.train:main",
+            "eval_command = src.eval:main",
+        ]
+    },
+)
--- setup.py
+++ setup.py
@@ -2,32 +2,14 @@
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = [
-    "dill>=0.3",
-    "hydra-core",
-    "matplotlib>=3.5",
-    "numpy>=1.21",
-    "pandas>=1.3",
-    "pyarrow",
-    "pydantic",
-    "pytorch-lightning",
-    "seaborn>=0.12.2",
-    "scikit_learn>=1.0",
-    "scipy>=1.7",
-    "sqlalchemy>=1.4",
-    "timer>=0.2",
-    "tqdm>=4.64",
-    "wandb>=0.12",
-]
-
 setup(
     name="src",
     version="0.0.1",
-    description="Developing a encoder transformer arcitekture for",
+    description="Describe Your Cool Project",
     author="",
     author_email="",
-    url="https://github.com/MoustHolmes/IceCubeEncoderTransformer", 
-    install_requires=INSTALL_REQUIRES,
+    url="https://github.com/user/project",
+    install_requires=["pytorch-lightning", "hydra-core"],
     packages=find_packages(),
     # use this to customize global commands available in the terminal after installing the package
     entry_points={
