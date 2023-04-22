8d7f0e9867618d5a5e26b1a8c15a26a7927e2f63
MoustHolmes <moust.holmes@gmail.com>
2023-04-22 17:08:53 +0200

    post merge fix

--- setup.py
+++ setup.py
@@ -2,13 +2,31 @@
 
 from setuptools import find_packages, setup
 
+INSTALL_REQUIRES = [
+    "dill>=0.3",
+    "hydra-core",
+    "matplotlib>=3.5",
+    "numpy>=1.21",
+    "pandas>=1.3",
+    "pyarrow",
+    "pydantic",
+    "pytorch-lightning",
+    "seaborn>=0.12.2",
+    "scikit_learn>=1.0",
+    "scipy>=1.7",
+    "sqlalchemy>=1.4",
+    "timer>=0.2",
+    "tqdm>=4.64",
+    "wandb>=0.12",
+]
+
 setup(
     name="src",
     version="0.0.1",
-    description="Describe Your Cool Project",
-    author="",
-    author_email="",
-    url="https://github.com/user/project",
+    description="Encoder Transformer model for use on data from the IceCube Observatory",
+    author="Moust Holmes",
+    author_email="Moust.holmes@gmail.com",
+    url="https://github.com/MoustHolmes/IceCubeEncoderTransformer",
     install_requires=["pytorch-lightning", "hydra-core"],
     packages=find_packages(),
     # use this to customize global commands available in the terminal after installing the package
