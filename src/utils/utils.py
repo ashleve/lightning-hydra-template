07ce4b798eb2ccc7f93dd24708a1aef43f0b0fe8
Alex Morehead <alex.morehead@gmail.com>
2023-09-09 08:11:34 -0500

    Update function arguments typing (#603)

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,6 @@
 import warnings
 from importlib.util import find_spec
-from typing import Any, Callable, Dict, Tuple
+from typing import Any, Callable, Dict, Optional, Tuple
 
 from omegaconf import DictConfig
 
@@ -95,12 +95,12 @@ def task_wrapper(task_func: Callable) -> Callable:
     return wrap
 
 
-def get_metric_value(metric_dict: Dict[str, Any], metric_name: str) -> float:
+def get_metric_value(metric_dict: Dict[str, Any], metric_name: Optional[str]) -> Optional[float]:
     """Safely retrieves value of the metric logged in LightningModule.
 
     :param metric_dict: A dict containing metric values.
-    :param metric_name: The name of the metric to retrieve.
-    :return: The value of the metric.
+    :param metric_name: If provided, the name of the metric to retrieve.
+    :return: If a metric name was provided, the value of the metric.
     """
     if not metric_name:
         log.info("Metric name is None! Skipping metric value retrieval...")
