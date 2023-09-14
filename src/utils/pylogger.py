462909d5613609d73c08da03b34aeafb12ac0b8b
Mattie Tesfaldet <mattie@meta.com>
2023-09-14 16:29:53 -0700

    Adding 'extra' kwarg to RankedLogger.

--- src/utils/pylogger.py
+++ src/utils/pylogger.py
@@ -1,21 +1,30 @@
 import logging
+from typing import Mapping, Optional
 
 from lightning_utilities.core.rank_zero import rank_prefixed_message, rank_zero_only
 
 
 class RankedLogger(logging.LoggerAdapter):
-    def __init__(self, name: str = __name__, rank_zero_only: bool = False) -> None:
+    """A multi-GPU-friendly python command line logger."""
+
+    def __init__(
+        self,
+        name: str = __name__,
+        rank_zero_only: bool = False,
+        extra: Optional[Mapping[str, object]] = None,
+    ) -> None:
         """Initializes a multi-GPU-friendly python command line logger that logs on all processes
         with their rank prefixed in the log message.
 
         :param name: The name of the logger. Default is ``__name__``.
         :param rank_zero_only: Whether to force all logs to only occur on the rank zero process. Default is `False`.
+        :param extra: (Optional) A dict-like object which provides contextual information. See `logging.LoggerAdapter`.
         """
         logger = logging.getLogger(name)
-        super().__init__(logger)
+        super().__init__(logger=logger, extra=extra)
         self.rank_zero_only = rank_zero_only
 
-    def log(self, level, msg, rank=None, *args, **kwargs) -> None:
+    def log(self, level: int, msg: str, rank: Optional[int] = None, *args, **kwargs) -> None:
         """Delegate a log call to the underlying logger, after prefixing its message with the rank
         of the process it's being logged from. If `'rank'` is provided, then the log will only
         occur on that rank/process.
