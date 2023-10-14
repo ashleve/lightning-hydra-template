1fed7e5056dd98cdb03f352d0918cfb546d5f9c0
Rafay Khan <rafayak16@gmail.com>
2023-10-14 15:14:11 +0500

    pre-commit fixes

--- src/utils/rich_utils.py
+++ src/utils/rich_utils.py
@@ -6,7 +6,7 @@ import rich.syntax
 import rich.tree
 from hydra.core.hydra_config import HydraConfig
 from lightning_utilities.core.rank_zero import rank_zero_only
-from omegaconf import ListConfig, DictConfig, OmegaConf, open_dict
+from omegaconf import DictConfig, ListConfig, OmegaConf, open_dict
 from rich.prompt import Prompt
 
 from src.utils import pylogger
